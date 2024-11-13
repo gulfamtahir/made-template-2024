b8a7ae5d7c88f7f488416ae2d39935f79aefb3b8
Muhammad Gulfam Tahir <muhammadgulfamtahir@Muhammads-MBP.fritz.box>
2024-11-14 00:38:40 +0100

    important info regarding kaggle api

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -3,5 +3,14 @@
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Reminder: Make sure you have set up your Kaggle API credentials before running this script.
+#Go to your Kaggle account page.
+#Under the “API” section, click “Create New API Token”. This will download a kaggle.json file containing your API credentials.
+# The 'kaggle.json' file should be placed in the following directory:
+# On Linux/Mac: ~/.kaggle/
+# On Windows: C:\Users\<Your Username>\.kaggle\
+
+
 # Run the Python pipeline script
 python3 pipeline.py
